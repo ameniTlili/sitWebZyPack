432c82e89551761a9fd8461062afd0730e8628f6
ameniTlili <ameni1tlili@gmail.com>
2020-01-18 17:55:14 +0100

    Add files via upload

---
+++ devis_sugar.php
@@ -0,0 +1,42 @@
+<?php
+		   if(isset($_POST)&& !empty($_POST['nom'])&& 
+		      !empty($_POST['email'])&& 
+		      !empty($_POST['tel'])&& 
+		      !empty($_POST['adr'])){
+		      extract($_POST);
+		      $destinataire ='ameni1tlili@gmail.com';
+		      $company="ZY PACK COMPANY \n".$nom." , merci pour votre choix";
+		      $message="Vos informations : \n \n";
+		      $from= "from :". $nom ." , num tel :" . $tel ." , mail : " . $email;
+		      $body="Nom du client :" . $nom."\n" ."Numero de tel :" . $tel ."\n" ."Email : " . $email ;
+		      $message .= $body ."\n \n Vos choix : \n";
+		      
+		      $body .= "\n" ."Article : sachet de sucre";
+		      $message .= "\n" ."Article : sachet de sucre";
+		      $body .= "\n" ."Lieu de livraison : ".$lieu;
+		      $message .= "\n" ."Lieu de livraison : ".$lieu;
+		      $body .= "\n" ."Qantité " . $qt;
+		      $message .= "\n" ."Qantité " . $qt;
+		      if($type =="goblet_standard") {
+		      	$body .="\n" ."Type : standard"; 
+		      	$message .="\n" ."Type : standard"; 
+		      	}
+		      else 
+		      {
+		      	$body .="\n" ."Type : personalisé" ;
+		      	$message .="\n" ."Type : personalisé" ;
+		      	$body .="\n" ."Nbr couleurs : ".$nbr_couleur ;
+		      	$message .="\n" ."Nbr couleurs : ".$nbr_couleur ;
+		      	$body .="\n" ."Description : ".$description ;
+		      	$message .="\n" ."Description : ".$description ;
+		      	
+		      
+		      }
+		     
+		      $mail=mail($destinataire,$from,$body,$from);
+		      $mail=mail($email,$from,$message,$company);
+		      if($mail)echo"<script type='text/javascript'>alert('email envoye avec succee')</script>"
+		      ;else echo"<script type='text/javascript'>alert('echec')</script>";
+		      }else echo"<script type='text/javascript'>alert('chaines vides')</script>";
+		      echo "<META http-equiv='refresh' content='0;URL=index.html'>";
+		?>
